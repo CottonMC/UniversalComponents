dd9d95e7d9a1a8455264649f4e30979c02d0e2c5
Meredith Espinosa <goclonefilms@gmail.com>
2020-03-17 23:07:16 -0700

    add injector for LBA item attributes, verbump

--- src/main/java/io/github/cottonmc/component/mixin/lba/MixinItemAttributes.java
+++ src/main/java/io/github/cottonmc/component/mixin/lba/MixinItemAttributes.java
@@ -2,10 +2,13 @@ package io.github.cottonmc.component.mixin.lba;
 
 import alexiil.mc.lib.attributes.CustomAttributeAdder;
 import alexiil.mc.lib.attributes.DefaultedAttribute;
+import alexiil.mc.lib.attributes.ItemAttributeList;
 import alexiil.mc.lib.attributes.item.FixedItemInv;
 import alexiil.mc.lib.attributes.item.ItemAttributes;
 import alexiil.mc.lib.attributes.item.compat.FixedInventoryVanillaWrapper;
 import alexiil.mc.lib.attributes.item.compat.FixedSidedInventoryVanillaWrapper;
+import alexiil.mc.lib.attributes.misc.LimitedConsumer;
+import alexiil.mc.lib.attributes.misc.Reference;
 import io.github.cottonmc.component.UniversalComponents;
 import io.github.cottonmc.component.compat.lba.AttributeWrapper;
 import io.github.cottonmc.component.item.InventoryComponent;
@@ -17,10 +20,12 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SidedInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Function;
@@ -80,5 +85,13 @@ public class MixinItemAttributes {
 			}
 
 		});
+	}
+
+	@Inject(method = "appendItemAttributes", at = @At("HEAD"))
+	private static <T> void injectComponentAdder(Reference<ItemStack> ref, LimitedConsumer<ItemStack> access, ItemAttributeList<T> list, Function<FixedItemInv, T> convertor, CallbackInfo info) {
+		ItemStack stack = ref.get();
+		if (UniversalComponents.INVENTORY_COMPONENT.maybeGet(stack).isPresent()) {
+			list.add(convertor.apply(new AttributeWrapper(UniversalComponents.INVENTORY_COMPONENT.get(stack))));
+		}
 	}
 }
