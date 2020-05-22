66393a7a8950b1e40b4903eba0eaea08bf1a0f51
Meredith Espinosa <goclonefilms@gmail.com>
2020-05-21 23:12:13 -0700

    fix item capacitor component

--- src/main/java/io/github/cottonmc/component/energy/impl/ItemCapacitorComponent.java
+++ src/main/java/io/github/cottonmc/component/energy/impl/ItemCapacitorComponent.java
@@ -2,21 +2,22 @@ package io.github.cottonmc.component.energy.impl;
 
 import io.github.cottonmc.component.UniversalComponents;
 import io.github.cottonmc.component.energy.CapacitorComponent;
+import io.github.cottonmc.component.energy.type.EnergyType;
 import nerdhub.cardinal.components.api.ComponentType;
 import nerdhub.cardinal.components.api.component.Component;
 import nerdhub.cardinal.components.api.util.ItemComponent;
 import net.minecraft.nbt.CompoundTag;
 
 public class ItemCapacitorComponent extends SimpleCapacitorComponent implements ItemComponent<CapacitorComponent> {
-	private ComponentType<CapacitorComponent> type;
+	private ComponentType<CapacitorComponent> componentType;
 
-	public ItemCapacitorComponent(int maxEnergy) {
-		this(maxEnergy, UniversalComponents.CAPACITOR_COMPONENT);
+	public ItemCapacitorComponent(int maxEnergy, EnergyType type) {
+		this(maxEnergy, type, UniversalComponents.CAPACITOR_COMPONENT);
 	}
 
-	public ItemCapacitorComponent(int maxEnergy, ComponentType<CapacitorComponent> type) {
-		super(maxEnergy);
-		this.type = type;
+	public ItemCapacitorComponent(int maxEnergy, EnergyType type, ComponentType<CapacitorComponent> componentType) {
+		super(maxEnergy, type);
+		this.componentType = componentType;
 	}
 
 	@Override
@@ -26,6 +27,6 @@ public class ItemCapacitorComponent extends SimpleCapacitorComponent implements 
 
 	@Override
 	public ComponentType<CapacitorComponent> getComponentType() {
-		return type;
+		return componentType;
 	}
 }
